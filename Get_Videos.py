410be92ad96f552379f497032e5942949d3dbfef
Marcus Lim <mj_lim0802@yahoo.com>
2023-07-19 16:38:55 -0500

    Cleaned up data, added description field

--- Get_Videos.py
+++ Get_Videos.py
@@ -6,84 +6,188 @@ import random
 import cv2
 import numpy as np
 import easyocr
-
-api_key = 'AIzaSyB0jjGAaJ867GAzM6dyog3DggN2vX6vYWg'
-tesseract_path = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-
-
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-request = youtube.channels().list(
-        part="contentDetails",
-        id="UCHWbZM3BIGgZksvXegx_h3w",
-        maxResults=500,
-    )
-
-response = request.execute()
-playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-
-next_page_token = None
-videos = []
-bad_videos = []
-
-while 1:
-    response = youtube.playlistItems().list(
-        playlistId = playlist_id,
-        part = 'snippet',
-        maxResults = 50,
-        pageToken = next_page_token
-    ).execute()
-
-    videos += response['items']
-    next_page_token=response.get('nextPageToken')
-    if next_page_token is None:
-        break
+import csv
+from pymongo import MongoClient
+import json
+import torch
 
 
-for i in range(len(videos)):
-    video_url = videos[i]['snippet']['resourceId']['videoId']
-    thumbnail_url = videos[i]['snippet']['thumbnails']['high']['url']
-    urllib.request.urlretrieve(thumbnail_url, "thumbnail.png")
+def get_videos(videos):
+    good_videos = []
+    count = 1
+    for i in range(len(videos)):
+        video_id = videos[i]['snippet']['resourceId']['videoId']
+        thumbnail_url = videos[i]['snippet']['thumbnails']['high']['url']
+        description = videos[i]['snippet']['description']
+        urllib.request.urlretrieve(thumbnail_url, "thumbnail.png")
+        
+        
 
-    image = cv2.imread('thumbnail.png')
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        image = cv2.imread('thumbnail.png')
+        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    reader = easyocr.Reader(['en'])
-    results = reader.readtext(image, detail=0)
+        reader = easyocr.Reader(['en'], gpu=True)
+        results = reader.readtext(image, detail=0, )
 
-    price = ""
+        price = ""
 
-    try:
-        string = results[0][1:].lower()
-        string = string.replace('/', '')
-        string = string.replace(' ', '')
-        string = string.replace('.','')
-        string = string.replace(',','')
-        if 'million' in string:
+        try:
+            string = results[0][1:].lower()
+            string = string.replace('/', '')
             string = string.replace(' ', '')
             string = string.replace('.','')
-            string = string.replace('million', '000000')
+            string = string.replace(',','')
+            string = string.replace('+','')
+            if 'million' in string:
+                string = string.replace(' ', '')
+                string = string.replace('.','')
+                string = string.replace('million', '000000')
+                price = int(string)
+
             price = int(string)
-        else:
-            price = int(string)
-            with open('GoodVideos.csv', 'r+') as file:
-                data = str(price)+','+video_url + '\n'
-                contents = file.read()
-                if data in contents:
-                    print("Already accounted for")
-                else:
-                    file.write(data)
-                    file.close()
-    except:
-        print("Wrong?")
-        print(results)
-        with open('ProblemVideos.txt', 'r+') as file:
-            contents = file.read()
-            if thumbnail_url in contents:
-                print("Already accounted for")
-                pass
+            item = {
+                'id' : count,
+                'videoID' : video_id,
+                'price' : price,
+                'description' : description
+            }
+            good_videos.append(item)
+            count += 1
+        except:
+            pass
+            # with open('ProblemVideos.txt', 'r+') as file:
+            #     contents = file.read()
+            #     if thumbnail_url in contents:
+            #         print("Already accounted for")
+            #         pass
+            #     else:
+            #         print(results)
+            #         cv2.imshow('problem', image)
+            #         cv2.waitKey(0)
+            #         file.write(thumbnail_url+'\n')
+            #         file.close()
+    print(count)
+    return good_videos
+
+
+
+def fetchYoutube():
+    api_key = 'AIzaSyB0jjGAaJ867GAzM6dyog3DggN2vX6vYWg'
+
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    request = youtube.channels().list(
+            part="contentDetails",
+            id="UCHWbZM3BIGgZksvXegx_h3w",
+            maxResults=500,
+        )
+
+    response = request.execute()
+    playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    next_page_token = None
+    videos = []
+    bad_videos = []
+
+    while 1:
+        response = youtube.playlistItems().list(
+            playlistId = playlist_id,
+            part = 'snippet',
+            maxResults = 50,
+            pageToken = next_page_token
+        ).execute()
+
+        videos += response['items']
+        next_page_token=response.get('nextPageToken')
+        if next_page_token is None:
+            break
+
+    return videos
+
+def cleanDesc(good_videos):
+    for i in range(len(good_videos)):
+        desc = good_videos[i]['description'].splitlines()
+        for j in range(len(desc)):
+            if "Specs:" in desc[j]:
+                newDesc = ""
+                newDesc+= desc[j] + "\n"
+                if "Price" not in desc[j+1]:
+                    newDesc+= desc[j+1] + "\n"
+                if "Price" not in desc[j+2]:
+                    newDesc+= desc[j+2] + "\n"
+                if "Price" not in desc[j+3]:
+                    newDesc+= desc[j+3] + "\n"
+                good_videos[i]['description'] = newDesc
+                break
             else:
-                file.write(thumbnail_url+'\n')
-                file.close()
+                good_videos[i]['description'] = ""
+        print(good_videos[i])
+
+    return good_videos
+
+
+
+
+
+
+def addToDB(listOfObj):
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['appraisle']
+    col = db['videos']
+
+    for item in listOfObj:
+        col.insert_one(item)
+    
+
+
+
+videos = fetchYoutube()
+goodVideos = get_videos(videos)
+for item in goodVideos:
+    print(item['id'])
+
+cleaned = cleanDesc(goodVideos)
+addToDB(cleaned)
+
+# for i in range(len(test)):
+#     if "Specs:" in test[i]:
+#         newDesc+= test[i]
+#         newDesc+= test[i+1]
+#         newDesc+= test[i+2]
+#         newDesc+= test[i+3]
+#         goodVideos[1]['description'] = newDesc
+#         break
+
+# print(goodVideos[1])
+
+
+
+
+
+# videos = fetchYoutube()
+# good_videos = get_videos(videos)
+
+
+# for i in range(len(videos)):
+#     obj = {
+#         'id' : i,
+#         'videoID' : videos[i]['snippet']['resourceId']['videoId'],
+#         'description' : videos[i]['snippet']['description'],
+#     }
+
+        
+#     listOfObj.append(obj)
+
+# item = listOfObj[0]
+
+# desc = item['description'].splitlines()
+    
+
+
+# for item in listOfObj:
+#     with open('description.txt', 'a+', encoding='utf-8') as f:
+#         f.write(item)
+#         f.close()
+
 
